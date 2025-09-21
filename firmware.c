dcf94bf0a78253cb349e5f9b9189bb304c6819d7
namedummy568-stack <namedummy568@gmail.com>
2025-09-21 15:27:23 +0530

    feat: Implement QAM modulation scheme

--- firmware.c
+++ firmware.c
@@ -2,7 +2,14 @@
 // Initial firmware code
 #include <stdio.h>
 
+// QAM Modulation Scheme Implementation
+void qam_modulate() {
+    // ... QAM modulation logic ...
+    printf("QAM Modulation Implemented\n");
+}
+
 int main() {
     printf("High-Speed Modem Firmware - Initial Version\n");
+    qam_modulate();
     return 0;
 }
